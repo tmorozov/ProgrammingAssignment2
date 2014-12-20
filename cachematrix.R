edc44acb5f198c73b7a68cfa658e4138052864d2
Timur Morozov <radist@desant.biz>
2014-12-20 11:41:06 +0200

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly 
+## Those are pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix: 
+## This function creates a special "matrix" object that can cache its inverse.
+## It returns the list with function for 
+## 1. set the value of the matrix (set)
+## 2. get the value of the matrix (get)
+## 3. set the value of the inverse (setinverse)
+## 4. get the value of the inverse (getinverse)
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## s - holds inverse of matrix
   s <- NULL
+  ## set function stores matrix value in x
   set <- function(y) {
     x <<- y
     s <<- NULL
   }
+  ## get function returns matrix value
   get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
+  ## setinverse - stores inverse value
+  setinverse <- function(inverse) s <<- inverse
+  ## getinverse - returns cahced inverse value
+  getinverse <- function() s
+  
+  ## return list of functions
   list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve: 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+## If the inverse has already been calculated, then the inverse from the cache is returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  s <- x$getsolve()
+  ## read cached inverse value
+  s <- x$getinverse()
+  ## check if it's not empty
   if(!is.null(s)) {
     message("getting cached data")
     return(s)
   }
+  ## if no chaced inverse value preset - calculate it
   data <- x$get()
+  ## solve function is used for calculation of inverse
   s <- solve(data, ...)
-  x$setsolve(s)
+  ## save calculated inverse value
+  x$setinverse(s)
+  ## return inverse value
   s
 }
